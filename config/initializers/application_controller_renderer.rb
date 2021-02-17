03eb34f3238d65cda05188bab03bfb593ec88d97
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-02-17 18:02:53 +0900

    劇団一覧サイトの実装

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'ded3d2262df5a200fbbe9c36070b95aed3f8dc3fcb395f29ae86e6756d774c3f40bd24a0ba7b9f694d556e0106570cf44f2c1dfe60f56f6449f8ce26e62e57c2'
