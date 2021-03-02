92a7e6bf31919c6a9b8fa98cd13e69aae974733d
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-03-02 14:40:56 +0900

    公演一覧のレストア

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,8 +1,10 @@
 class PerformancesController < ApplicationController
   def index
+    @performances = Performance.all
   end
 
   def show
+    @performance = Performance.find(params[:id])
   end
 
   def new
@@ -10,5 +12,27 @@ class PerformancesController < ApplicationController
   end
 
   def edit
+    @performance = Performance.find(params[:id])
+
   end
+
+  def create
+    @performance = Performance.new(performance_params)
+    @performance.user_id = current_user.id
+    @performance.save
+    redirect_to performance_path(@performance)
+  end
+
+  def update
+    @performance = Performance.find(params[:id])
+    @performance.update(performance_params)
+    redirect_to performance_path(@performance)
+  end
+
+
+  private
+  def performance_params
+    params.require(:performance).permit(:title, :body, :image)
+  end
+
 end
