035b2e9f1b34d143ae3606cf653a64157aee5d65
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-03-02 14:45:43 +0900

    削除機能の実装

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -11,4 +11,11 @@ class PerformancesController < ApplicationController
 
   def edit
   end
+
+  def destroy
+    performance = Performance.find(params[:id])
+    performance.destroy
+    redirect_to performances_path
+  end
+
 end
